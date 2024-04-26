ffd4545d9e3c662fbccd3b0e9289b3d108694659
vikashsingh02 <107889207+vikashsingh02@users.noreply.github.com>
2024-04-26 16:28:09 +0530

    added ig like feature file to branch

---
+++ likefeature/script.js
@@ -0,0 +1,13 @@
+let con= document.querySelector(".content");
+let love = document.querySelector("i");
+con.addEventListener("dblclick",function(){
+    love.style.transform = "translate(-50%) scale(1)";
+    love.style.opacity = "0.8";
+    setTimeout(function(){
+        love.style.opacity = "0";
+    },1000)
+    setTimeout(function(){
+        love.style.transform = "translate(-50%) scale(0)";
+    },2000)
+
+})
